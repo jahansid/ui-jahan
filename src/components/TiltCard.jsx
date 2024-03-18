42658d7a6cb2b911ae63931bf11fc050566c58be
jahan siddiqui <raj918833@gmail.com>
2024-03-18 18:05:20 +0530

    card animation done

--- src/components/TiltCard.jsx
+++ src/components/TiltCard.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable react/prop-types */
+// /* eslint-disable react/prop-types */
 import { useRef } from "react";
 
 function TiltCard({
@@ -9,25 +9,40 @@ function TiltCard({
   const boundingRef = useRef(null);
 
   return (
-    
-      <div className="flex flex-col [perspective:800px]">
-        <div
-          
-         className=" group relative grid w-[260px] grid-rows-[200px_120px_40px] rounded-md bg-[#C6BFFA] p-4 text-[#282d86] transition-transform  hover:[transform:rotateX(35deg)_rotateY(15deg)_scale(1.1)] ">
-          <figure className="rounded-md mix-blend-multiply bg-[#413678]  " />
-          <div className=" mt-6  ">
-            <h3 className=" uppercase text-lg font-bold">{name}</h3>
-            <h3 className=" capitalize text-md font-medium ">{title}</h3>
-          </div>
-          <footer className="">
-            <p className="flex justify-center rounded-sm border-2 border-current px-1 py-px text-[9px] uppercase">
-              {date}
-            </p>
-          </footer>
-          <div className="pointer-events-none absolute inset-0 group-hover:bg-[radial-gradient(at_var(--x)_var(--y),rgba(255,255,255,0.3)_20%,transparent_80%)]" />
-      </div>
+    <div className="flex flex-col [perspective:800px]">
+      <div
+       onMouseLeave={() => (boundingRef.current = null)}
+        onMouseEnter={(event) => {
+          boundingRef.current = event.currentTarget.getBoundingClientRect();
+        }}
+        onMouseMove={(event) => {
+          if (!boundingRef.current) return;
+          const x = event.clientX - boundingRef.current.left;
+          const y = event.clientY - boundingRef.current.top;
+          const xPercentage = x / boundingRef.current.width;
+          const yPercentage = y / boundingRef.current.height;
+          const xRotation = (xPercentage - 0.5) * 20;
+          const yRotation = (0.5 - yPercentage) * 20;
+
+          event.currentTarget.style.setProperty("--x-rotation", `${yRotation}deg`);
+          event.currentTarget.style.setProperty("--y-rotation", `${xRotation}deg`);
+          event.currentTarget.style.setProperty("--x", `${xPercentage * 100}%`);
+          event.currentTarget.style.setProperty("--y", `${yPercentage * 100}%`);
+        }}
+        className=" group relative grid w-[260px] grid-rows-[200px_120px_40px] rounded-md bg-[#C6BFFA] p-4 text-[#282d86] transition-transform  ease-out hover:[transform:rotateX(var(--x-rotation))_rotateY(var(--y-rotation))_scale(1.1)] "
+      >
+        <figure className="rounded-md mix-blend-multiply bg-[#413678]  " />
+        <div className=" mt-6  ">
+          <h3 className=" uppercase text-lg font-bold">{name}</h3>
+          <h3 className=" capitalize text-md font-medium ">{title}</h3>
         </div>
-    
+        <footer className="">
+          <p className="flex justify-center rounded-sm border-2 border-current px-1 py-px text-[9px] uppercase">
+            {date}
+          </p>
+        </footer>
+      </div>
+    </div>
   );
 }
 
