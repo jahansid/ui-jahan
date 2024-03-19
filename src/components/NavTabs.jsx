93652dbbe43059b64cf04e7f4c9f06c3b6782862
jahan siddiqui <raj918833@gmail.com>
2024-03-20 02:45:47 +0530

    animation done for navtabs. alldone.

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { useState } from "react";
 
 function NavTabs() {
@@ -6,22 +7,33 @@ function NavTabs() {
     { id: 2, label: "Portfolio" },
     { id: 3, label: "Contact Us" },
     { id: 4, label: "Services" },
-    { id: 5, label: "Business" },
+    { id: 5, label: "Work" },
   ];
   const [activeTab, setActiveTab] = useState(tabs[0].id);
   return (
-    <div className="grid place-content-center min-h-screen bg-indigo-950 text-white">
-      <div className="">
-        <div className="flex space-x-4 capitalize">
+    <div className="grid place-content-center min-h-screen bg-zinc-900 ">
+      <div className=" border-2 border-white rounded-2xl px-4 py-5">
+        <div className="flex space-x-4 ">
           {tabs.map((tab) => (
             <button
               key={tab.id}
               onClick={() => setActiveTab(tab.id)}
               className={` ${
-                activeTab === tab.id ? "bg-violet-600" : "hover:opacity-80"
-              }  rounded-full px-3 py-1.5 text-sm font-semibold outline-2 outline-sky-400 focus-visible:outline `}
+                activeTab === tab.id ? "" : "hover:text-white/50"
+              } relative  rounded-full capitalize px-3 py-1.5 text-sm text-white transition font-semibold outline-2 outline-sky-400 focus-visible:outline `}
             >
-              {tab.label}
+              <span className=" relative z-10 mix-blend-exclusion">
+                {tab.label}
+              </span>
+
+              {activeTab === tab.id && (
+                <motion.div
+                  layoutId="active-pill"
+                  style={{ borderRadius: 9999 }}
+                  transition={{ type: "spring", duration: 0.6 }}
+                  className="absolute inset-0 bg-white"
+                />
+              )}
             </button>
           ))}
         </div>
